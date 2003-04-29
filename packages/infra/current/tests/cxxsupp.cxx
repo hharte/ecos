5c036c8b9166ff451c9727f637a1b1c0eeb34620
Jonathan Larmour <jlarmour@redhat.com>
2003-04-29 22:38:15 +0000

    * tests/cxxsupp.cxx: don't include <pkgconf/kernel.h> as it's not 	needed. 	(cyg_start): CYGINT_ISO_MALLOC needs a #if test.

--- packages/infra/current/tests/cxxsupp.cxx
+++ packages/infra/current/tests/cxxsupp.cxx
@@ -48,7 +48,6 @@
 //####DESCRIPTIONEND####
 //==========================================================================
 
-#include <pkgconf/kernel.h>
 #include <pkgconf/hal.h>
 #include <pkgconf/isoinfra.h>
 
@@ -139,7 +138,7 @@ cyg_start( void )
     pure->impure_fun1();
     pure->inline_fun1();
 
-#ifdef CYGINT_ISO_MALLOC
+#if CYGINT_ISO_MALLOC
     Derived *derived2 = new Derived(2);
     Pure *pure2 = derived2;
     
